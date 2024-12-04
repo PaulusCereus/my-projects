a0ee4a138bf3f4c448c5f93e8f7b1c2b6a915e1d
Pavel Glancev <pauluscereus@gmail.com>
2024-12-04 19:09:49 +0300

    Added example with ppython-object to JSON-string

--- json/main_json.py
+++ json/main_json.py
@@ -3,10 +3,24 @@ import json
 # Преобразуем строку JSON в Python-объект
 # ------------------------------------------------------------
 
-json_string = '{"name": "Alice", "age":"25", "is_student":false}'
+# json_string = '{"name": "Alice", "age":"25", "is_student":false}'
 
-data = json.loads(json_string)
+# data = json.loads(json_string)
 
-print(data["name"])
-print(data["age"])
-print(data["is_student"])
+# print(data["name"])
+# print(data["age"])
+# print(data["is_student"])
+
+# Преобразуем Python-объект в строку JSON
+# ------------------------------------------------------------
+
+python_data = {
+    "name": "Leha",
+    "age": 5,
+    "gruz": 200,
+    "is_student": True,
+    "skills": [],
+}
+
+json_string = json.dumps(python_data, indent=4)
+print(json_string)
