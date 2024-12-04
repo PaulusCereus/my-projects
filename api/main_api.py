1343928964a85ad334e5e9a998be631dba5f4fdf
Pavel Glancev <pauluscereus@gmail.com>
2024-12-04 20:25:47 +0300

    First example of api with quotes - ERROR

--- api/main_api.py
+++ api/main_api.py
@@ -1,18 +1,29 @@
+# import requests
+# import json
+# import pprint
+
+# response = requests.get("https://randomuser.me/api")
+# if response.ok:
+#     data = response.json()
+#     user = data["results"][0]
+#     print(f"Имя: {user['name']['first']}")
+#     print(f"Страна: {user['location']['country']}")
+#     print("-" * 30)
+#     print("Printing data")
+#     pprint.pprint(data)
+#     print("Printing user")
+#     pprint.pprint(user)
+# else:
+#     print("Ошибка получения данных")
+
+# ----------------------------------------------------
+
 import requests
-import json
-import pprint
 
-response = requests.get("https://randomuser.me/api")
-if response.ok:
+response = requests.get("https://api.quotable.io/random")
+
+if response.status_code == 200:
     data = response.json()
-    user = data["results"][0]
-    print(f"Имя: {user['name']['first']}")
-    print(f"Страна: {user['location']['country']}")
-    print("-" * 30)
-    print("Printing data")
-    pprint.pprint(data)
-    print("Printing user")
-    pprint.pprint(user)
+    print(f"Случайная цитата: '{data['content']}' - {data['author']}")
 else:
-    print("Ошибка получения данных")
-
+    print("Не удалось получить цитату")
