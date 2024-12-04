ab641b3b322386dcdbf4d83d61e63849ee5b5c5d
Pavel Glancev <pauluscereus@gmail.com>
2024-12-04 19:20:40 +0300

    Example about loading json file

--- json/main_json.py
+++ json/main_json.py
@@ -14,13 +14,30 @@ import json
 # Преобразуем Python-объект в строку JSON
 # ------------------------------------------------------------
 
-python_data = {
-    "name": "Leha",
-    "age": 5,
-    "gruz": 200,
-    "is_student": True,
-    "skills": [],
-}
+# python_data = {
+#     "name": "Leha",
+#     "age": 5,
+#     "gruz": 200,
+#     "is_student": True,
+#     "skills": [],
+# }
 
-json_string = json.dumps(python_data, indent=4)
-print(json_string)
+# json_string = json.dumps(python_data, indent=4)
+# print(json_string)
+
+# Загрузка данных из JSON файла
+# ------------------------------------------------------------
+
+# Путь до файла указываем от папки my-projects, так как мы находимся в ней
+# и путь ищется от неё до файла data.json
+
+# В данном случае мы применяем контекстный менеджер для открытия файла
+# на чтение ("r") (("read")) и сохраняем указатель на файл в виде file
+# Использование контекстного менеджера удобно, так как при завершении работы
+# нам не нужно вручную закрывать файл
+with open("./json/data.json", "r") as file:
+    data = json.load(file)
+
+print(data)
+print(data["skills"])
+print(data["name"])
